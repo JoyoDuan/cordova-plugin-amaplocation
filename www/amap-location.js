89fc33a12dce64dd5c71f344cacd802c2b1bac25
JoyoDuan <joyoduan@163.com>
2020-05-27 11:49:41 +0800

    高德定位插件

---
+++ www/amap-location.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+
+// 这里的命令AMapLocationPlugin与plugin.xml无关，用abc都可以，module.exports用于require('')
+function AMapLocationPlugin() {}
+
+AMapLocationPlugin.prototype.getLocation = function(successCallback, errorCallback) {
+	exec(successCallback, errorCallback, "AMapLocationPlugin", "getLocation", []);
+};
+
+module.exports = new AMapLocationPlugin();
