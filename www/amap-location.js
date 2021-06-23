cd8ad779b61a12604ef5f4c577f0e668cb7d0bae
JoyoDuan <JoyoDuan@163.com>
2021-06-23 15:39:32 +0800

    支持连续定位

--- www/amap-location.js
+++ www/amap-location.js
@@ -3,8 +3,11 @@ var exec = require('cordova/exec');
 // 这里的命令AMapLocationPlugin与plugin.xml无关，用abc都可以，module.exports用于require('')
 function AMapLocationPlugin() {}
 
-AMapLocationPlugin.prototype.getLocation = function(successCallback, errorCallback) {
-	exec(successCallback, errorCallback, "AMapLocationPlugin", "getLocation", []);
+AMapLocationPlugin.prototype.getLocation = function(successCallback, errorCallback, locationOptions) {
+	exec(successCallback, errorCallback, "AMapLocationPlugin", "getLocation", [
+		locationOptions.isOnceLocation,
+		locationOptions,interval
+	]);
 };
 
 module.exports = new AMapLocationPlugin();
